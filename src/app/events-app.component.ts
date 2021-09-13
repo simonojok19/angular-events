3ed032d696a4744959de5e7000f125f6c8fc69a3
simonojok19 <simonojok19@gmail.com>
2021-09-13 05:25:49 +0300

    added the stylesheets and images

--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   // tslint:disable-next-line:component-selector
   selector: 'events-app',
-  template: `<h2>Hello World</h2>`,
+  template: `
+    <h2>Hello World</h2>
+    <img src="/assets/images/basic-shield.png" alt="" />
+  `,
 })
 export class EventsAppComponent {
   title = 'angular-events';
