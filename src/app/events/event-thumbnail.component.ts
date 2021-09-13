4e5c2651f52980ced161fde25a56fd093d41d697
simonojok19 <simonojok19@gmail.com>
2021-09-13 06:37:43 +0300

    now passing data via property binding

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -1,38 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   // tslint:disable-next-line:component-selector
   selector: 'events-thumbnail',
   template: `
-    <div>
-      <h1>Upcoming Angular Events</h1>
-      <hr />
-      <div class="well hoverwell thumbnail">
-        <h2>{{ event.name }}</h2>
-        <div>Date: {{ event.date }}</div>
-        <div>Time: {{ event.time }}</div>
-        <div>Price: \${{ event.price }}</div>
-        <div>
-          <span>Location: {{ event.location.address }}</span>
-          <span>&nbsp;</span>
-          <span>{{ event.location.city }}, {{ event.location.country }}</span>
-        </div>
+    <div class="well hoverwell thumbnail">
+      <h2>{{ event.name }}</h2>
+      <div>Date: {{ event.date }}</div>
+      <div>Time: {{ event.time }}</div>
+      <div>Price: \${{ event.price }}</div>
+      <div>
+        <span>Location: {{ event.location.address }}</span>
+        <span>&nbsp;</span>
+        <span>{{ event.location.city }}, {{ event.location.country }}</span>
       </div>
     </div>
   `,
 })
 export class EventThumbnailComponent {
-  event: any = {
-    id: 1,
-    name: 'Angular Connect',
-    date: '9/26/2036',
-    time: '10:00 am',
-    price: 599.9,
-    imageUrl: '/assets/images/angularconnect-shield.png',
-    location: {
-      address: '1057 DT',
-      city: 'London',
-      country: 'England',
-    },
-  };
+  @Input() event: any;
 }
