9e2b1090ef5e0edef49a440778aadf5bb0d6fd3c
simonojok19 <simonojok19@gmail.com>
2021-09-13 22:47:24 +0300

    added 404 component

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { EventsListComponent } from './events/events-list.component';
 import { EvenDetailsComponent } from './events/event-details/even-details.component';
 import { CreateEventComponent } from './events/create-event.component';
+import { Error404Component } from './errors/404.component';
 
 export const appRoutes: Routes = [
   { path: 'events', component: EventsListComponent },
   { path: 'events/new', component: CreateEventComponent },
   { path: 'events/:id', component: EvenDetailsComponent },
+  { path: '404', component: Error404Component },
   { path: '', redirectTo: '/events', pathMatch: 'full' },
 ];
