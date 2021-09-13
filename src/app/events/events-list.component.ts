62fd1fb72704ba3990e71bef1a34ce90d498e63d
simonojok19 <simonojok19@gmail.com>
2021-09-13 06:27:13 +0300

    finished the component template

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -6,7 +6,33 @@ import { Component } from '@angular/core';
   template: `
     <div>
       <h1>Upcoming Angular Events</h1>
+      <hr />
+      <div class="well hoverwell thumbnail">
+        <h2>{{ event.name }}</h2>
+        <div>Date: {{ event.date }}</div>
+        <div>Time: {{ event.time }}</div>
+        <div>Price: \${{ event.price }}</div>
+        <div>
+          <span>Location: {{ event.location.address }}</span>
+          <span>&nbsp;</span>
+          <span>{{ event.location.city }}, {{ event.location.country }}</span>
+        </div>
+      </div>
     </div>
   `,
 })
-export class EventsListComponent {}
+export class EventsListComponent {
+  event: any = {
+    id: 1,
+    name: 'Angular Connect',
+    date: '9/26/2036',
+    time: '10:00 am',
+    price: 599.9,
+    imageUrl: '/assets/images/angularconnect-shield.png',
+    location: {
+      address: '1057 DT',
+      city: 'London',
+      country: 'England',
+    },
+  };
+}
