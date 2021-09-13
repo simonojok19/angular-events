88fdb45193876e14204395adeb548eaa2bee4db1
simonojok19 <simonojok19@gmail.com>
2021-09-13 22:53:20 +0300

    added the routing functionalities

---
+++ src/app/events/event-details/event-route.guard.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+} from '@angular/router';
+import { EventsService } from '../shared/events.service';
+
+@Injectable()
+export class EventRouteGuard implements CanActivate {
+  constructor(private eventsService: EventsService, private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): boolean {
+    const eventExist = !!this.eventsService.getEvent(
+      Number(route.paramMap.get('id'))
+    );
+
+    if (!eventExist) {
+      this.router
+        .navigate(['/events'])
+        .then((result) => console.log('Routed to /events', result));
+    }
+
+    return true;
+  }
+}
