54f735be755d7816f92339ec49c8c700a188d726
simonojok19 <simonojok19@gmail.com>
2021-09-13 07:55:01 +0300

    cleaned the app

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -14,17 +14,10 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
         <span>&nbsp;</span>
         <span>{{ event.location.city }}, {{ event.location.country }}</span>
       </div>
-      <button class="btn btn-primary" (click)="handleClickMe()">
-        Click Me!
-      </button>
     </div>
   `,
 })
 export class EventThumbnailComponent {
   @Input() event: any;
   @Output() eventClick = new EventEmitter();
-
-  handleClickMe(): void {
-    this.eventClick.emit(this.event.name);
-  }
 }
