e471f57088912db251cf41be920bb2c1e3c12df5
simonojok19 <simonojok19@gmail.com>
2021-09-13 22:09:12 +0300

    added the template

--- src/app/events/event-details/even-details.component.ts
+++ src/app/events/event-details/even-details.component.ts
@@ -2,5 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   templateUrl: '.even-details.component.html',
+  styles: [
+    `
+      .container {
+        padding-left: 20px;
+        padding-right: 20px;
+      }
+
+      .event-image {
+        height: 100px;
+      }
+    `,
+  ],
 })
 export class EvenDetailsComponent {}
