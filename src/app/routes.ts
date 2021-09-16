ad5416b8cb039356a0a9714e2f2d883e599aa25b
simonojok19 <simonojok19@gmail.com>
2021-09-16 22:18:04 +0300

    load children and dynamic import

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -24,4 +24,8 @@ export const appRoutes: Routes = [
   },
   { path: '404', component: Error404Component },
   { path: '', redirectTo: '/events', pathMatch: 'full' },
+  {
+    path: 'user',
+    loadChildren: () => import('./user/user.module').then((m) => m.UserModule),
+  },
 ];
