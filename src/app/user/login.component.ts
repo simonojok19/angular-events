21d1109793419b4de56fa7db4015f43933c418c7
simonojok19 <simonojok19@gmail.com>
2021-09-17 09:02:44 +0300

    added validations

--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -4,6 +4,15 @@ import { Router } from '@angular/router';
 
 @Component({
   templateUrl: './login.component.html',
+  styles: [
+    `
+      em {
+        float: right;
+        color: #e05c65;
+        padding-left: 10px;
+      }
+    `,
+  ],
 })
 export class LoginComponent {
   userName: string;
