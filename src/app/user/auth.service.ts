35fa3946d22533c4146b97ff789f1f40991a75fc
simonojok19 <simonojok19@gmail.com>
2021-09-17 08:32:28 +0300

    added the authervice

---
+++ src/app/user/auth.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { IUser } from './user.model';
+
+@Injectable()
+export class AuthService {
+  currentUser: IUser;
+
+  loginUser(userName: string, password: string): void {
+    this.currentUser = {
+      id: 1,
+      userName,
+      firstName: 'John',
+      lastName: 'Papa',
+    };
+  }
+}
